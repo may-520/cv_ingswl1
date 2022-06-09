ef4dcee3d2ef9024d4b9d2139c6a5a3ffb382553
Usuario <Usuario@Usuario-PC>
2022-06-09 14:47:16 -0500

    version 5

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -9,6 +9,67 @@ public class Cv_ejercicio {
         System.out.println("versión 1");
         System.out.println("versión 2");
         System.out.println("versión 3");
+        System.out.println("version 5");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
