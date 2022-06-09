965e6aae9dfd0738f8d3737e35dd4cac712da8e7
may-520 <85308679+may-520@users.noreply.github.com>
2022-06-09 14:34:50 -0500

    Update Cv_ejercicio.java

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -7,6 +7,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         System.out.println("versión 0");
         System.out.println("versión 1");
+        System.out.println("versión 2");
     }
     
 }
